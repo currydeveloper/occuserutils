65dfde0e9f6d62db76ad88a108853d52f4b18d76
krishna vangala <currydeveloper@gmail.com>
2024-06-21 08:48:38 -0500

    Added angular 18 and build folder to sync.

--- src-ui/src/app/app.module.ts
+++ src-ui/src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StepperComponent } from './components/stepper/stepper.component';
+import { MaterialModule } from 'material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StepperComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,StepperComponent]
 })
 export class AppModule { }
---
+++ src-ui/src/app/components/stepper/stepper.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stepper',
+  templateUrl: './stepper.component.html',
+  styleUrls: ['./stepper.component.css'],
+  
+})
+
+export class StepperComponent {
+
+}
